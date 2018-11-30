0434619f7b9228252f033538c04cabba238bc21a
Behrouz Hosseini(Work) <behrouz.hosseini.81@hotmail.com>
2018-11-30 10:12:11 +0100

    add first sql script

---
+++ Important-query.sql
@@ -0,0 +1,10 @@
+SELECT UEAP.ID, PR.Name, US.emailAddress FROM `project` AS PR 
+LEFT OUTER JOIN `user_entityactionpermission` AS UEAP ON UEAP.ObjectID = PR.ProjectId AND UEAP.EntityActionId = 18
+LEFT OUTER JOIN `user` AS US ON UEAP.UserId = US.UserId AND US.EmailAddress = "fn@scapetechnologies.com"
+WHERE PR.IsDeleted = false
+SELECT UEAP.ID, PR.Name, US.emailAddress FROM `project` AS PR 
+LEFT OUTER JOIN `user_entityactionpermission` AS UEAP ON UEAP.ObjectID = PR.ProjectId AND UEAP.EntityActionId = 18
+LEFT OUTER JOIN `user` AS US ON UEAP.UserId = US.UserId AND US.EmailAddress = "fn@scapetechnologies.com"
+WHERE PR.IsDeleted = false
+
+ORDER BY PR.Name
