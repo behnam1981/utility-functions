5b1f80ca002298531376cb14a8135d5f7e291397
Behrouz Hosseini(Work) <behrouz.hosseini.81@hotmail.com>
2018-11-30 10:45:42 +0100

    Change the is deleted to ture

--- Important-query.sql
+++ Important-query.sql
@@ -5,6 +5,6 @@ WHERE PR.IsDeleted = false
 SELECT UEAP.ID, PR.Name, US.emailAddress FROM `project` AS PR 
 LEFT OUTER JOIN `user_entityactionpermission` AS UEAP ON UEAP.ObjectID = PR.ProjectId AND UEAP.EntityActionId = 18
 LEFT OUTER JOIN `user` AS US ON UEAP.UserId = US.UserId AND US.EmailAddress = "fn@scapetechnologies.com"
-WHERE PR.IsDeleted = false
+WHERE PR.IsDeleted = true
 
 ORDER BY PR.Name
